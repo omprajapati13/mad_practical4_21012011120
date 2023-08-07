9ba72dd6d0583ccc92cf6c94a4ee2de89a8fd94c
student <student@gnu.ac.in>
2023-08-07 10:43:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical4_21012011120/RegistrationActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mad_practical4_21012011120
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        val loginBtn:Button = findViewById(R.id.loginButton)
+        val re
+    }
+}
