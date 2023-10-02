a8e113708a4047d67ed35548f4d538d490f1d409
DELL <OMVARIYA71@GMAIL.COM>
2023-10-02 14:22:46 +0530

    ...

--- app/src/main/java/com/example/mad_practical4_21012011120/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical4_21012011120/RegistrationActivity.kt
@@ -1,14 +1,33 @@
 package com.example.mad_practical4_21012011120
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        val loginTxt:TextView= findViewById(R.id.loginText)
+        val registerBtn:Button = findViewById(R.id.registerButton)
+
+        loginTxt.setOnClickListener {
+            Intent(this@RegistrationActivity, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        registerBtn.setOnClickListener {
+
+            Toast.makeText(this@RegistrationActivity, "Registered Successfully",Toast.LENGTH_LONG).show()
+
+            Intent(this@RegistrationActivity, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
 
 
 
